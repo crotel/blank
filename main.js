3759ae0a4ff1b240deac52987711e236c1181b58
CROTEL <crotel@users.noreply.github.com>
2023-05-17 11:53:51 +0800

    20230517_115347

--- main.js
+++ main.js
@@ -8,6 +8,16 @@ window.$ls = window.localStorage;
 window.$ss = window.sessionStorage;
 
 window.classOf = (o)=>Object.prototype.toString.call(o).slice(8, -1);
+window.task = (e)=>{
+return new Promise((resolve, reject) => {
+ if(classOf(e)!="Function"){
+  resolve(e);
+ } else {
+  resolve(new Function(e)())
+ }
+  
+});
+};
 window.delay = (milliseconds)=>new Promise(resolve=>setTimeout(resolve, milliseconds));
 window.sha = async(arg=Date.now())=>await btoa(await String.fromCharCode(...new Uint8Array(await crypto.subtle.digest("SHA-512", (new TextEncoder().encode(decodeURIComponent(arg))))))).replace(/[\/=+]/g, "");
 window.plainPaste = function(event){
@@ -20,13 +30,15 @@ window.plainPaste = function(event){
   selection.getRangeAt(0).insertNode(document.createTextNode(paste));
   selection.collapseToEnd();
 };
-const originalConsoleLog = console.log;
+// const originalConsoleLog = console.log;
+Array.from(Object.keys(console)).filter(r=>r!="memory").map(r=>{window.originalConsoleLog = console[r].bind(console);})
 function consoleLogProxy() { 
  originalConsoleLog.apply( console , arguments ); 
- document.getElementById( "container" ).innerHTML = `${ Array.prototype.slice.apply( arguments , [] ) .join( " " ) }`; /*<br>${ document.getElementById( "container" ).innerHTML }*/
+ document.getElementById( "container" ).innerHTML = `${ Array.prototype.slice.apply( arguments , [] ) .join( " " ) }<br>${ document.getElementById( "container" ).innerHTML }`; /*<br>${ document.getElementById( "container" ).innerHTML }*/
 };
-console.log = consoleLogProxy;
-window.log = console.log.bind(console);
+Array.from(Object.keys(console)).filter(r=>r!="memory").map(r=>{console[r] = consoleLogProxy});
+// console.log = consoleLogProxy;
+// window.log = console.log.bind(console);
 $id("main").oninput = (evt)=>{
  // log(evt.target.innerText);
  location.hash = encodeURIComponent(evt.target.innerHTML);
@@ -41,14 +53,16 @@ window.addEventListener("hashchange", (evt) => {
  // console.log(evt.newURL.replace(location.origin+"/#",""))
  return new Function(decodeURIComponent(evt.newURL.replace(location.origin+"/#","")))()
 });
-window.onload = (evt)=>{
- evt.preventDefault();
+// window.onload = (evt)=>{
+//  evt.preventDefault();
  if(location.hash!=""){
   // jsbeautifier.beautify('your JavaScript string')
   $id("main").innerHTML = decodeURIComponent(location.hash.replace("#",""));
-  return new Function($id("main").textContent)()
+   // window.call($id("main").textContent)()
+  // task(new Function($id("main").textContent))
+  task(new Function($id("main").textContent)())
  }
-}
+// }
 
 // $id("main").designMode = "on";
 //  function _htmlNode(id,cl,node,t=Date.now()){
